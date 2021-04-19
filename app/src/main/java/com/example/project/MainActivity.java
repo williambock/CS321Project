77224d81e689acfc1c41018eed15a2bca3109a1e
williambock <57406617+williambock@users.noreply.github.com>
2021-04-19 18:15:06 -0400

    pathch-3 added to master

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -21,9 +21,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project.ui.main.SectionsPagerAdapter;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project.ui.main.SectionsPagerAdapter;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button settingsBtn;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference messagesRef = db.collection("messages");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +81,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void loadPost() {
+        messagesRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                            Post post = documentSnapshot.toObject(Post.class);
+                            String message = post.getPost();
+                            String userID = post.getUserID();
+                            System.out.println(message + "\n" + userID + "\n");
+                        }
+                    }
+                });
     }
 
     private void newPostActivity() {
