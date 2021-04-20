b5245bfeeee78b8c47a7860f0f4d1db2ee996c7f
yzhang54 <71297348+yzhang54@users.noreply.github.com>
2021-04-20 18:24:26 -0400

    Add files via upload

--- app/src/main/java/com/example/project/SettingsActivity.java
+++ app/src/main/java/com/example/project/SettingsActivity.java
@@ -1,15 +1,24 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    private SharedPreferences SP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
@@ -25,9 +34,7 @@ public class SettingsActivity extends AppCompatActivity {
                     .commit();
         }
         ActionBar actionBar = getSupportActionBar();
-     
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         // check frame layout is empty or not.
         if (findViewById(R.id.idFrameLayout) != null) {
@@ -36,18 +43,39 @@ public class SettingsActivity extends AppCompatActivity {
             }
 
             getFragmentManager().beginTransaction().add(R.id.idFrameLayout, new RealSetting()).commit();
+
         }
     }
-        public boolean onOptionsItemSelected(MenuItem item){
+
+    public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivityForResult(intent, 0);
         return true;
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+            CheckBoxPreference checkboxPref = (CheckBoxPreference) getPreferenceManager().findPreference(getString(R.string.night));
+            //    CheckBoxPreference nightMode = findPreference(getString(R.string.night));
+            checkboxPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    boolean yes = (boolean) newValue;
+                    if (yes) {
+                        getListView().setBackgroundColor(Color.parseColor("#222222"));
+                    }
+                    else {
+                        getListView().setBackgroundColor(Color.parseColor("#ffffff"));
+                    }
+
+                    return true;
+                }
+            });
         }
+
     }
 }
